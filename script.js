c45443fee0dc9e88ca507c9c7211408f0dfea1c1
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-06 22:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
-function palindrome(str){
+function isPalindrome(s) {
+            // Remove non-alphanumeric characters and convert to lowercase
+            s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
-}
-module.exports = palindrome
+            // Compare the original string with its reverse
+            return s === s.split('').reverse().join('');
+        }
+
+        function checkPalindrome() {
+            const inputString = document.getElementById('inputString').value;
+            const resultElement = document.getElementById('result');
+
+            if (isPalindrome(inputString)) {
+                resultElement.textContent = '"' + inputString + '" is a palindrome.';
+            } else {
+                resultElement.textContent = '"' + inputString + '" is not a palindrome.';
+            }
+		}
